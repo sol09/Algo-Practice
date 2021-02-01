e462f05a62e941dd8eb2056dc17f49999046d58f
sol09 <55572177+sol09@users.noreply.github.com>
2021-02-01 21:27:57 +0900

    Update 13.java
    #Math #String

--- Pending/13.java
+++ Pending/13.java
@@ -1,50 +1,46 @@
-// 13
+// 13. Roman to Integer - #Math #String
 
 class Solution {
     public int romanToInt(String s) {
-        char[] rom = {'M','D','C','L','X','V','I'};
-        int [] num = {1000,500,100,50,10,5,1};
-        int answer = 0; 
+        // 로마숫자 정수로 바꾸기
+        // 큰 수부터 씀
+        int re = 0;
+        int i = 0;
         
-        for(int i= 0; i<s.length();++i) {
-                for(int j = 0; j<rom.length;j++) {
-                    if(s.charAt(i) == rom[j]) {
-                        if(i+1 < s.length()) {
-                            if(s.charAt(i) == 'C' && s.charAt(i+1) == 'M') {
-                                answer += 900;
-                                i++;
-                                break;
-                            }
-                            else if(s.charAt(i) == 'C' && s.charAt(i+1) == 'D') {
-                                answer += 400;
-                                i++;
-                                break;
-                            }
-                            else if(s.charAt(i) == 'X' && s.charAt(i+1) == 'C') {
-                                answer += 90;
-                                i++;
-                                break;
-                            }
-                            else if(s.charAt(i) == 'X' && s.charAt(i+1) == 'L') {
-                                answer += 40;
-                                i++;
-                                break;
-                            } 
-                            else if(s.charAt(i) == 'I' && s.charAt(i+1) == 'X') {
-                                answer += 9;
-                                i++;
-                                break;
-                            } 
-                            else if(s.charAt(i) == 'I' && s.charAt(i+1) == 'V') {
-                                answer += 4;
-                                i++;
-                                break;
-                            }
-                        }
-                        answer += num[j];
-                    }
-                }
+        while(i<s.length()) {
+            switch(s.charAt(i)) {
+                case 'M':
+                    re+=1000;
+                    break;
+                case 'D':
+                    re+=500;
+                    break;
+                case 'C':
+                    if(i+1<s.length() && s.charAt(i+1)=='M') {re+=900; i++;}
+                    else if(i+1<s.length() && s.charAt(i+1)=='D') {re+=400; i++;}
+                    else re+=100;
+                    break;
+                case 'L':
+                    re+=50;
+                    break;
+                case 'X':
+                    if(i+1<s.length() && s.charAt(i+1)=='C') {re+=90; i++;}
+                    else if(i+1<s.length() && s.charAt(i+1)=='L') {re+=40; i++;}
+                    else re+=10;
+                    break;
+                case 'V':
+                    re+=5;
+                    break;
+                case 'I':
+                    //if(i+1!=s.length())
+                    if(i+1<s.length() && s.charAt(i+1)=='X') {re+=9; i++;}
+                    else if(i+1<s.length() && s.charAt(i+1)=='V') {re+=4; i++;}
+                    else re+=1;
+                    break;
             }
-        return answer;
+            i++;
+        }
+        
+        return re;
     }
 }
