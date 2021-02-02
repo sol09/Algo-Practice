cea11e8d99e795298156932d4da9b3e284a91323
sol09 <55572177+sol09@users.noreply.github.com>
2021-02-02 22:03:30 +0900

    Create 112.java

---
+++ Pending/112.java
@@ -0,0 +1,39 @@
+// 112.
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean hasPathSum(TreeNode root, int targetSum) {
+        boolean flag = false;
+        
+        if(root==null)
+            return false;
+        if(root.left==null && root.right==null) {
+            if(root.val==targetSum) return true;
+            else return false;
+        }
+        
+        int sum = targetSum - root.val;
+        if(root.left!=null) {
+            flag = hasPathSum(root.left, sum);
+        }
+        if(root.right!=null && flag==false) {
+            flag = hasPathSum(root.right, sum);
+        }
+        
+        return flag;
+    }
+}
