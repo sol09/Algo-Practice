0c74ba03540885019a607b80367b786e78e9ddef
sol09 <55572177+sol09@users.noreply.github.com>
2021-02-04 17:21:33 +0900

    Create 121.java
    Array, Dynamic Programming

---
+++ Accepted/121.java
@@ -0,0 +1,21 @@
+// 121. Best Time to Buy and Sell Stock
+
+class Solution {
+    public int maxProfit(int[] prices) {
+        int min = prices[0];
+	    int index=0;
+        int result=0;
+	        
+	    for (int i=1;i<prices.length;i++){
+	        if (min>prices[i])
+	            min = prices[i];
+            else {
+                // 수익의 최댓값 구하기
+                int tmp = prices[i] - min;
+                if(tmp>result)
+                    result = tmp;
+            }
+	    }
+        return result;
+    }
+}
