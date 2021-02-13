127b6bce0c95756958bf4a4f32b2ded729adf9fd
sol09 <55572177+sol09@users.noreply.github.com>
2021-02-14 00:11:58 +0900

    Array, Math, Bit Manipulation

--- Accepted/268.java
+++ Accepted/268.java
@@ -1,2 +1,16 @@
 // 268. Missing Number
 
+class Solution {
+    public int missingNumber(int[] nums) {
+        int n = nums.length;
+        int sum = 0;
+        
+        for(int i=0;i<n;i++) {
+            sum += nums[i];
+        }
+        
+        int re = n*(n+1)/2;
+        
+        return (re-sum);
+    }
+}
