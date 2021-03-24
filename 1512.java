26d62b5dea142fb2a67d8b2a5b9358ce103832f2
sol09 <55572177+sol09@users.noreply.github.com>
2021-03-25 00:02:08 +0900

    Array, Hash Table, Math

--- 1512.java
+++ 1512.java
@@ -6,7 +6,7 @@ class Solution {
         int all = 0;
         
         for(int n : nums) {
-            if(!map.containsValue(n)) {
+            if(!map.containsKey(n)) {
                 map.put(n,1);
             }
             else {
@@ -24,3 +24,6 @@ class Solution {
         return all;
     }
 }
+
+
+/* 1번 문제랑 비슷한 느낌. 비슷하게 풀었더니 마찬가지로 시간은 좋게 나왔는데 공간이 별로 안 좋음. 시간 100%, 공간 26.25%. 다른 방법 찾아볼 것. -210324- */
