c7058337af02de144407d5ad0020677e84d24742
sol09 <55572177+sol09@users.noreply.github.com>
2021-03-28 23:57:16 +0900

    Update 1037.java

--- 1037.java
+++ 1037.java
@@ -22,3 +22,28 @@ class Solution {
 }
 
 /* 미완성. 내일 마저 풀 것. -210327- */
+
+
+// 210328
+
+class Solution {
+    public boolean isBoomerang(int[][] points) {
+        // 세 점이 한 직선 위에 존재하는지를 return
+        // Hint1. 면적이 0이 아니면 true, 방정식으로는 풀 수 없을지?
+        
+        double a = Math.sqrt(Math.pow(points[0][0]-points[1][0],2)
+                             + Math.pow(points[0][1]-points[1][1]),2);
+        double b = Math.sqrt(Math.pow(points[1][0]-points[2][0],2)
+                             + Math.pow(points[1][1]-points[2][1]),2);
+        double c = Math.sqrt(Math.pow(points[2][0]-points[0][0],2)
+                             + Math.pow(points[2][1]-points[0][1]),2);
+        
+        double s = (a+b+c)/2;
+        double Scale = Math.sqrt(s*(s-a)*(s-b)*(s-c));
+        
+        if(Scale!=0)
+            return true;
+        else
+            return false;
+    }
+}
