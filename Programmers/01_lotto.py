c09854ef9a7a99ab94717673aa6fb5c0391e9ea2
YESOL <55572177+sol09@users.noreply.github.com>
2022-02-24 22:01:21 +0900

    Create 01_lotto.py

---
+++ Programmers/01_lotto.py
@@ -0,0 +1,19 @@
+def solution(lottos, win_nums): 
+    answer = [] 
+    wcount = zcount = 0 
+    for n in lottos: 
+        if n in win_nums: 
+            wcount += 1 
+        if n == 0: 
+            zcount += 1
+    if wcount+zcount >= 2: 
+        answer.append(7-wcount-zcount) 
+    else: 
+        answer.append(6) 
+    if wcount >= 2: 
+        answer.append(7-wcount)
+    else: 
+        answer.append(6) 
+    return answer
+
+# https://edorrr.tistory.com/40
