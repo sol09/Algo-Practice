cb4d9b115819d66447de6188bf39b77423088d91
YESOL <55572177+sol09@users.noreply.github.com>
2022-06-02 22:22:09 +0900

    String

--- Accepted/14.py
+++ Accepted/14.py
@@ -1,7 +1,6 @@
 # 14. Longest Common Prefix
 
 # Trial 1
-
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
         prefix = ""
@@ -14,12 +13,12 @@ class Solution:
             prefix += l
         return prefix
       
-# Runtime 56.07%, Memory 49.63% - 중첩 반복문을 써서 썩 마음에 드는 코드는 아님. 효율도 썩 좋게 안 나왔고.. 
-#                                 근데 이 방법 말고는 생각이 안 나서 다른 코드 보면서 새로 알게 되는 부분 정리해봐야겠다.
+# Runtime 56.07%, Memory 49.63%
+# 중첩 반복문을 써서 썩 마음에 드는 코드는 아님. 효율도 썩 좋게 안 나왔고.. 
+# 근데 이 방법 말고는 생각이 안 나서 다른 코드 보면서 새로 알게 되는 부분 정리해봐야겠다.
 
 
 # Reference : https://leetcode.com/problems/longest-common-prefix/discuss/6918/Short-Python-Solution
-
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
         if not strs:
@@ -34,3 +33,4 @@ class Solution:
 # 일단 내 코드는 아닌데 보면서 공부할 만 할 것 같아서 추가함.
 # 눈에 띄는 부분은 (1) if문에서의 리스트 null 확인 (2) min() 사용 (3) enumerate() 사용 < 인데, 
 # (1)과 (3)은 이미 아는 내용이고 종종 사용했음에도 문제 풀 때는 떠올리지 못 했다. (2)는 함수 속성에 key 를 추가하는 것을 잘 몰랐어서 그 내용을 정리해보려고 함.
+# 재밌는 점은 좋은 코드지만 중첩반복문 쓴 건 동일하고, 효율이 딱히 좋게 나오진 않았다는 점. 아직 easy 단계라서 작은 변수에도 크게 작용하는 듯.
