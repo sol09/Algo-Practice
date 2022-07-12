9b7a2168a4dc30e326c10cf9e721ac3710c83e7f
YESOL <55572177+sol09@users.noreply.github.com>
2022-07-12 14:46:36 +0900

    Update 21.py

--- 21.py
+++ 21.py
@@ -1,1 +1,11 @@
 # 21. Merge Two Sorted Lists
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        if not list1 or not list2:
